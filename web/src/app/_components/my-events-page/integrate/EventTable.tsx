3d681b742e5432aca3a031d1d6719caf18455b7f
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-07-25 17:00:07 -0300

    revalidate the home page every 1h

--- web/src/app/_components/my-events-page/integrate/EventTable.tsx
+++ web/src/app/_components/my-events-page/integrate/EventTable.tsx
@@ -5,7 +5,7 @@ export const EventTable = async () => {
   const events = await fetch(`${process.env.NEXT_SERVER_URL}/event`, {
     cache: "no-cache",
     next: {
-      revalidate: 60, // revalidate this page every 60 seconds
+      revalidate: 60 * 60, // revalidates this page every 1 hour
     },
   })
     .then((response) => response.json())
